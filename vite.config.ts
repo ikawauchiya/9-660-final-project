5ff3c82707851da2712d27e5b8e8b672bf6014ff
Inori Kawauchiya <inorik@mit.edu>
2024-12-04 06:03:50 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/9-660-final-project/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
